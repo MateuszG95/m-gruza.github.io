0778a3c9177a1d1dc23ff737ddf11c453b44e4bf
MateuszG95 <mat.gruza@gmail.com>
2020-02-22 19:05:11 +0100

    Initial commit

---
+++ js/sticky-nav.js
@@ -0,0 +1,25 @@
+<script src="jquery-1.11.3.min.js"></script>
+	
+<script>
+
+	$(document).ready(function() {
+	var NavY = $('#mainmenu').offset().top;
+	 
+	var stickyNav = function(){
+	var ScrollY = $(window).scrollTop();
+		  
+	if (ScrollY > NavY) { 
+		$('#mainmenu').addClass('sticky');
+	} else {
+		$('#mainmenu').removeClass('sticky'); 
+	}
+	};
+	 
+	stickyNav();
+	 
+	$(window).scroll(function() {
+		stickyNav();
+	});
+	});
+	
+</script>
